de9a44b3355fd89b4043dfa284fa83ba60dc1a90
Kevin Courter <kcourter@eoi.space>
2026-01-29 13:28:41 -0700

    update to use unique_ptr

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,13 @@
 int main( void ) {
     std::cout << "entering main" << std::endl;
 
-    Image *im = nullptr;
     try {
-        im = new Image( "/Users/kcourter/dev/upskill/data/test_images/standard/baboon.jpg" );
+        auto im = std::make_unique<Image>( "/Users/kcourter/dev/upskill/data/test_images/standard/baboon.jpg" );
         std::cout << "Image is: " << *im << std::endl;
+        im->write( "/Users/kcourter/dev/upskill/data/results/bletch.png", Image::ImgType::PNG );
     }
     catch ( const std::runtime_error& err ) {
         std::cerr << err.what() << std::endl;
     }
-
-    im->write( "/Users/kcourter/dev/upskill/data/results/bletch.png", Image::ImgType::PNG );
- 
-    delete im;
-
-
     return 0;
 }
